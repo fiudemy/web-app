cb292c3c904a7621f36c0b5f30c9b6fa208630e8
grizzoeh <gonzarizzoeh@gmail.com>
2023-10-18 18:16:18 -0300

    added marketplace course preview popup

--- fiudemy-web/src/modules/views/MarketPlace.js
+++ fiudemy-web/src/modules/views/MarketPlace.js
@@ -1,8 +1,7 @@
 import * as React from 'react';
 import Typography from '../components/Typography';
 import Paper from '@mui/material/Paper';
-import { CoursesGrid } from '../components/courses/CoursesGrid';
-import { Box } from '@mui/material';
+import { Box, Button, ButtonBase, CardMedia, Dialog, DialogActions, DialogContent, DialogTitle, Grid } from '@mui/material';
 import { FormControl, MenuItem, Select } from '@mui/material';
 import { useState } from 'react';
 
@@ -12,22 +11,57 @@ const cursos = [
     name : 'Matematicas',
     description : 'Aprende matematicas de una forma facil y divertida',
     category : 'Matematicas',
+    duration : 720
 
   },
   {
     name: 'Fisica',
     description : 'Aprende fisica de una forma facil y divertida',
     category : 'Fisica',
+    duration : 720
   },
   {
     name: 'Fisica',
     description : 'Aprende fisica de una forma facil y divertida',
     category : 'Fisica',
+    duration : 720
+
   },
   {
     name: 'Fisica',
     description : 'Aprende fisica de una forma facil y divertida',
     category : 'Fisica',
+    duration : 720
+
+  },
+  {
+    name : 'Matematicas',
+    description : 'Aprende matematicas de una forma facil y divertida',
+    category : 'Matematicas',
+    duration : 720
+
+
+  },
+  {
+    name: 'Fisica',
+    description : 'Aprende fisica de una forma facil y divertida',
+    category : 'Fisica',
+    duration : 720
+
+  },
+  {
+    name: 'Fisica',
+    description : 'Aprende fisica de una forma facil y divertida',
+    category : 'Fisica',
+    duration : 720
+
+  },
+  {
+    name: 'Fisica',
+    description : 'Aprende fisica de una forma facil y divertida',
+    category : 'Fisica',
+    duration : 720
+
   },
 ]
 
@@ -35,12 +69,72 @@ const categories = [
   'Matematicas', 'Fisica', 'Quimica', 'Biologia', 'Programacion'
 ]
 
-export const CourseHomeBox = ({course}) => {
+export const CourseMarketBox = ({course}) => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
-    <Paper sx={{ p: 2,  maxWidth: 250 }}>
-      <Typography variant="h6"> {course.name} </Typography>
-      <Typography>{course.description}</Typography>
-    </Paper>
+    <>
+      <ButtonBase onClick={handleOpen}>
+        <Paper sx={{ p: 2,  maxWidth: 250,  boxShadow: '2px 2px 4px rgba(0, 0, 0, 0.3)', cursor: 'pointer', textAlign: 'left'}}>
+          <Typography variant="h6"> {course.name} </Typography>
+          <Typography>{course.description}</Typography>
+        </Paper>
+      </ButtonBase>
+      <Dialog open={open} onClose={handleClose} maxWidth="md" fullWidth={true}>
+        <MarketPopupBody course={course} />
+        <DialogActions>
+          <Button onClick={handleClose}>Close</Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  );
+}
+
+const MarketPopupBody = ({course}) => {
+  return (
+    <>
+   <DialogTitle>{course.name}</DialogTitle>
+      <DialogContent>
+        <CardMedia
+          component="img"
+          height="200"
+          // image={course.image}
+          image={'https://images.shiksha.com/mediadata/images/articles/1681209899php8OqvF2.jpeg'}
+          alt={course.name}
+          sx={{ mt: 2 }}
+        />
+        <Typography variant="h6" sx={{ mt:6 }}>Descripcion</Typography>
+        <Typography>{course.description}</Typography>
+        <Typography variant="h6" sx={{ mt:6 }}>Duración</Typography>
+        <Typography>{course.duration + " horas"}</Typography>
+        <Button variant="contained" color="success" sx={{ mt: 6, width: '40%', color: '#fff' }}>
+          Pagar
+        </Button>
+      </DialogContent>
+    </>
+  );
+}
+
+
+const MarketCoursesGrid = ({courses}) => {
+  return (
+
+      <Grid container spacing={2} sx={{ mt: 2, ml: 3, mb: 6 }}> 
+        {courses.map((course, index) => (
+          <Grid item key={index} >
+            <CourseMarketBox course={course} />
+          </Grid>
+        ))}
+      </Grid>
+    
   );
 }
 
@@ -72,7 +166,7 @@ export default function MarketPlace() {
             ))}
           </Select>
         </FormControl>
-      <CoursesGrid courses={filteredCourses} />
+      <MarketCoursesGrid courses={filteredCourses} />
       </Box>
     </>
   );
