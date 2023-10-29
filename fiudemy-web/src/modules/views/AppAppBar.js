093d61278972e69627ccfa441c4e5d35e1b85366
mpiligaddi <mgaddi@fi.uba.ar>
2023-10-29 15:46:23 -0300

    Added logout buttom

--- fiudemy-web/src/modules/views/AppAppBar.js
+++ fiudemy-web/src/modules/views/AppAppBar.js
@@ -1,9 +1,14 @@
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button'; // Importamos el componente Button
 import Link from '@mui/material/Link';
+import List from '@mui/material/List';
+import Popover from '@mui/material/Popover';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle'; // Icono de usuario
 import * as React from 'react';
 import AppBar from '../components/AppBar';
 import Toolbar from '../components/Toolbar';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const rightLink = {
   fontSize: 16,
@@ -12,23 +17,89 @@ const rightLink = {
 };
 
 function AppAppBar({ showsSignInOptions = true }) {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleIconClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
     <div>
       <AppBar position="fixed">
         <Toolbar sx={{ justifyContent: 'space-between' }}>
-          <Box sx={{flex:1, display : 'flex', alignItems : 'center'}}>
-          </Box>
+        {!showsSignInOptions && (
+          <Box sx={{flex:1, display : 'flex', justifyContent : 'flex-end', alignItems: "center" }}>
           <Link
             variant="h6"
             underline="none"
             color="inherit"
-            href="/"
+            alignItems="flex-end"
             sx={{ fontSize: 24 }}
           >
             {'FIUDEMY'}
           </Link>
+          <IconButton
+            color="inherit"
+            onClick={handleIconClick}
+            aria-describedby={anchorEl ? 'user-menu' : undefined}
+          >
+          <AccountCircleIcon />
+          </IconButton>
+          <Popover
+            id="user-menu"
+            open={Boolean(anchorEl)}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'bottom',
+              horizontal: 'right',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+          >
+          <List>
+            <Link
+              color="inherit"
+              variant="h6"
+              underline="none"
+              href="/sign-in/"
+              sx={{
+                ...rightLink,
+                display: 'flex',
+                alignItems: 'flex-start',
+                color: 'black',
+                ml: 1,
+              }}
+            >
+              <LogoutIcon />
+              <Typography variant="body1" sx={{ ml: 1 }}>
+                {'Cerrar Sesión'}
+              </Typography>
+            </Link>
+          </List>
+          </Popover>
+          </Box>
+        )}
           {showsSignInOptions && (
-          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end' }}>
+          <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-end', alignItems: "center" }}>
+            <Box sx={{ flex: 1, display: 'flex', justifyContent: 'flex-start' }}>
+            <Link
+              variant="h6"
+              underline="none"
+              color="inherit"
+              href="/"
+              alignItems="flex-end"
+              sx={{ fontSize: 24 }}
+            >
+            {'FIUDEMY'}
+            </Link>
+            </Box>
             <Link
               color="inherit"
               variant="h6"
